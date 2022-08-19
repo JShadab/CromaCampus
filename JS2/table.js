4f250ce3c0d9d2c380598adc83ba595a321b3448
unknown <shadab105662@gmil.com>
2022-08-19 12:28:13 +0530

    DynamicTable 2

--- JS2/table.js
+++ JS2/table.js
@@ -22,26 +22,30 @@ const data = [
     }];
 
 
-addHeaderRow(table);
-addDataRow(table);
+addHeaderRow();
+createDataRows();
 
 body.appendChild(table)
 
+createButton();
 
-function addHeaderRow(table) {
+function addHeaderRow() {
     const headerRow = document.createElement('tr')
 
     const headerCol1 = document.createElement('th')
     headerCol1.innerHTML = "NAME"
     headerCol1.style.border = "1px solid red"
+    headerCol1.style.backgroundColor = "yellow"
 
     const headerCol2 = document.createElement('th')
     headerCol2.innerHTML = "AGE"
     headerCol2.style.border = "1px solid red"
+    headerCol2.style.backgroundColor = "yellow"
 
     const headerCol3 = document.createElement('th')
     headerCol3.innerHTML = "SALARY"
     headerCol3.style.border = "1px solid red"
+    headerCol3.style.backgroundColor = "yellow"
 
     headerRow.appendChild(headerCol1);
     headerRow.appendChild(headerCol2);
@@ -50,10 +54,11 @@ function addHeaderRow(table) {
     table.appendChild(headerRow);
 }
 
-function addDataRow(table) {
+function createDataRows() {
 
     for (emp of data) {
         const dataRow = document.createElement('tr')
+        dataRow.className = 'dataRow';
 
         const col1 = document.createElement('td')
         col1.innerHTML = emp.name;
@@ -74,4 +79,35 @@ function addDataRow(table) {
         table.appendChild(dataRow);
     }
 
+}
+
+function createButton() {
+
+    const btn = document.createElement('button');
+    btn.innerHTML = 'Add Row';
+
+    btn.onclick = addRow;
+
+    body.appendChild(document.createElement('br'))
+    body.appendChild(btn)
+}
+
+function addRow() {
+    const obj = {
+        "name": "Papa",
+        "age": "40",
+        "salary": "98765"
+    }
+
+    clearAllDataRows();
+    data.push(obj);
+    createDataRows();
+
+}
+
+function clearAllDataRows() {
+    const allRows = table.querySelectorAll('.dataRow');
+    for (row of allRows) {
+        table.removeChild(row);
+    }
 }
